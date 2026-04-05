ad59bf7a1be605a54b7e1dd38171c45eb68afd13
IsraelRodz <israel.ro1990@gmail.com>
2026-04-05 10:51:40 -0600

    Cometepo_82

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,5 +1,3 @@
-// src/layouts/Layout.tsx
-
 import { ReactNode } from 'react';
 import NavBar from '../Components/NavBar';
 
@@ -9,12 +7,11 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="font-sans text-gray-800 bg-gradient-to-br from-blue-50 to-white min-h-screen">
+    <div className="bg-black text-white min-h-screen font-sans">
       <NavBar />
-      {/* Espaciador para Navbar fijo */}
-      <div className="h-24"></div>
-      <main className="px-4 md:px-10 py-8 animate-fade-in">
-        <div className="max-w-6xl mx-auto space-y-14 bg-white/80 backdrop-blur-md p-6 md:p-10 rounded-3xl shadow-xl transition-all duration-500">
+
+      <main className="pt-24 px-4 md:px-10">
+        <div className="max-w-7xl mx-auto space-y-20">
           {children}
         </div>
       </main>
